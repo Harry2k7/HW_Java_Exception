6af6a9443f8c0c61bed1ce678c3d266e70a3eabb
igor <igorkudriy2k7@gmail.com>
2023-10-26 12:02:59 +0300

    final commit

--- WriteToFile.java
+++ WriteToFile.java
@@ -5,12 +5,7 @@ import java.io.IOException;
 public class WriteToFile {
     public void writeToFile(User user) throws IOException {
         try (FileWriter writer = new FileWriter(user.getSurname() + ".txt", true)) {
-            writer.write("<" + user.getSurname() + ">" + "<"
-                    + user.getName() + ">" + "<"
-                    + user.getPatronymic() + ">" + "<"
-                    + user.getDateOfBirth() + ">" + "<"
-                    + String.valueOf(user.getNumberOfTelephone()) + ">" + "<"
-                    + user.getGender().toString() + ">");
+            writer.write(user.toString());
             writer.write("\n");
             writer.flush();
         } catch (IOException e) {
